da29257460dae92e61ce58a7f567596bdd676ab3
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-17 19:24:02 +0530

    Update Valid  Parentheses.cpp

--- Week_3/Day_3/Valid  Parentheses.cpp
+++ Week_3/Day_3/Valid  Parentheses.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        
+        int i=1;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else if(s[i]==']') {
+                if(st.empty()) return false;
+                if(st.top()!='[') return false;
+                else st.pop();
+            }
+            else if(s[i]=='}') {
+                if(st.empty()) return false;
+                if(st.top()!='{') return false;
+                else st.pop();
 
+            }
+            else if (s[i]==')'){
+                if(st.empty()) return false;
+                if(st.top()!='(') return false;
+                else st.pop();
+            }   
+        
+        }
+        return st.empty();
+    }
+};
