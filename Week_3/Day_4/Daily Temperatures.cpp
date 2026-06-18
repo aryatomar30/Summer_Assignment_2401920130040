759112969a37d350b4eaf7059f875084b2f716d8
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-18 16:28:52 +0530

    Update Daily Temperatures.cpp

--- Week_3/Day_4/Daily Temperatures.cpp
+++ Week_3/Day_4/Daily Temperatures.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        std::stack<int>stk;
+        std::vector<int>res(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(!stk.empty()&&temperatures[stk.top()]<=temperatures[i]){
+                stk.pop();
+            }
+            if(!stk.empty()){
+                res[i]=stk.top()-i;
+            }
+            stk.push(i);
+        }
+        return res;
+    }
+};
