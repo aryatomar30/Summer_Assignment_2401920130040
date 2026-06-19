b7d8317ee9554992805279249d145b198931bca2
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-19 22:08:16 +0530

    Update Slinding Window Maximum.cpp

--- Week_3/Day_5/Slinding Window Maximum.cpp
+++ Week_3/Day_5/Slinding Window Maximum.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> sol;
+        int ctr = 0;
+        for (int i=0;i<n;i++){ 
+            // lets assume 1 as first element, if after 1 we receive a single bigger element then 1 gets popped itself
+            // but if we get smaller elements, then 1 at front, thus we will need to pop explicitly
+            while (!dq.empty() && dq.front()<=i-k) dq.pop_front();
+            while (!dq.empty() && nums[dq.back()]<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (i>=k-1) sol.push_back(nums[dq.front()]);
+            
+        }
+        return sol;      
+    }
+};
