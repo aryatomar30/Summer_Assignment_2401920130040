82536ccca3eeed1b7199ad9ad598cf1d28e42622
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-22 22:26:02 +0530

    Update Same Tree.cpp

--- Week_4/Day_1/Same Tree.cpp
+++ Week_4/Day_1/Same Tree.cpp
@@ -1,1 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* swap(TreeNode* root){
+        if (root==NULL){
+            return 0;
+        }
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        swap(root->left);
+        swap(root->right);
+        return root;
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        swap(root);
+        return root;
+    }
+};
