ffb564d8acabecca210986a75a243753d583f20b
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-22 22:23:12 +0530

    Update Maximum Depth of Binary Tree.cpp

--- Week_4/Day_1/Maximum Depth of Binary Tree.cpp
+++ Week_4/Day_1/Maximum Depth of Binary Tree.cpp
@@ -1,1 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    int traverse(struct TreeNode* temp){
+        if(temp==nullptr) return 0;
+        return 1+max(traverse(temp->right),traverse(temp->left));
+    }
+
+    int maxDepth(TreeNode* root) {
+        
+        int count=0;
+        count  = traverse(root);
+        return count;
+
+
+    }
+};
