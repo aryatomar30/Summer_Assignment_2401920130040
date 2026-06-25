cdf55cea743a469d52dc5d0af63a04c4446a00ab
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-26 00:23:49 +0530

    Update Lowest Comman Ancestor of a BST.cpp

--- Week_4/Day_3/Lowest Comman Ancestor of a BST.cpp
+++ Week_4/Day_3/Lowest Comman Ancestor of a BST.cpp
@@ -1,11 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == NULL) return NULL;
-        if(root->val == val) return root;
-        if(val < root->val) 
-            return searchBST(root->left,val);
-        else 
-            return searchBST(root->right,val);
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL){
+            return nullptr;
+        }
+
+        if(root == q || root == p){
+            return root;
+        }
+        //Case 1 if both Nodes less than root then go left
+        if(p->val<root->val && q->val<root->val){
+            TreeNode* leftAns =  lowestCommonAncestor(root->left, p,q);
+            if(leftAns) return leftAns;
+        }
+        //Case 2 if both Nodes greater than root then go right
+        else if(p->val > root->val && q->val > root->val){
+             TreeNode* rightAns =  lowestCommonAncestor(root->right, p,q);
+             if(rightAns) return rightAns;
+        }
+        
+        return root;
     }
 };
