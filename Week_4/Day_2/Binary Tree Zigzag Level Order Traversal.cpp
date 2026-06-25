41db41d759bd7c4dbc85c58f29722991757fb021
Arya Tomar <24115cn072@glbitm.ac.in>
2026-06-25 18:04:00 +0530

    Update Binary Tree Zigzag Level Order Traversal.cpp

--- Week_4/Day_2/Binary Tree Zigzag Level Order Traversal.cpp
+++ Week_4/Day_2/Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        q.push(root); // initialy push root element
+        bool stateLR= true; // flag to determine reverse the level
+        while(!q.empty()){
+            int n = q.size(); // size of current level
+            vector<int> level;
+            for(int i{};i<n;i++){ // traverse all ele of current level
+                TreeNode* node = q.front();
+                q.pop();
+                if(node){ // push next level
+                    level.push_back(node->val);
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+            if(!stateLR) reverse(level.begin(),level.end()); // alternate revese each level
+            stateLR = !stateLR;
+            if(level.size()>0)ans.push_back(level);
+        }
+        return ans;
+    }
+};
